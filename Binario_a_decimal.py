7bdba17150a5abc20bc2da2c81dad2ddd9bfc2ec
Mateo Nossa <jmnossah@academia.usbbog.edu.co>
2024-02-13 18:10:58 -0500

    Actividad 1

--- Binario_a_decimal.py
+++ Binario_a_decimal.py
@@ -8,17 +8,18 @@ def decimal_binario(bi):
     print(f'El número {bi} en decimal es: {suma}')
 
 
-num=input(str('Escriba el número en binario para obtener su valor en numeración en decimal:'))#Se toma como 'str' para analizarlo como una lista de caracteres 
+num=input(str('Escriba el número en binario para obtener su valor en numeración en decimal (Solo 1 y 0; no mas de 63 bits/dígitos):'))#Se toma como 'str' para analizarlo como una lista de caracteres 
 
 Estado=False #Se establece un estado para posterior comprobación de que el numero solo este compuesto de '1' y '0'
 for i in range(0,len(num)):#Se hace uso de la función 'len' para comenzar la comprobación da cada elemento
-    if num[i]=='1' or num[i]=='0':
+    if num[i]=='1' or num[i]=='0' :
         Estado=True# Si todos los elementos son '1' o '0' se da por valido para previa operación
     else:
-        print('Hay valores distintos de 1 y 0, verifique y vuelca a intentarlo.')
+        print('Hay valores distintos de 1 y 0, verifique y vuelca a intentarlo y/o el número es mayor a 63 bits/dígitos')
         Estado=False #Si encuentra algún valor distinto de '1' y '0' se rompe el ciclo (For) y el numero queda invitado para que no se ejecute el resto del programa
         break
 
-if Estado==True:
-    decimal_binario(num)#Se invoca la función  
-
+if Estado==True and len(num)<=63: #Comprueba el estado y valida la cantidad de dígitos
+    decimal_binario(num)#Se invoca la función
+else:
+    print('El número es mayor a 63 bits/dígitos') #Si no cumple la condición de los dígitos máximos
